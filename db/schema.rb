0b655d2d83f21ee54ec12a3055b275fca61e5a39
Nikki Veen <nikkiveen@gmail.com>
2017-04-02 09:34:32 -0500

    create images model/controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402143208) do
+ActiveRecord::Schema.define(version: 20170402143352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_images_on_project_id", using: :btree
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
